c8722e265bac759378193ddf58a7e90a303c0053
AwargawarRaju <aurgyaraju@gmail.com>
2024-10-25 08:14:39 +0530

    added

---
+++ js7.js
@@ -0,0 +1,69 @@
+
+// task:
+// ------
+// 1.Traffic Light System
+// Simulate a traffic light system:
+
+// If the light is red, log "Stop."
+// If the light is yellow, log "Get ready to move."
+// If the light is green, log "Go."
+// If the input doesn't match any color, log "Invalid input."
+var traffic = "light";
+ if(traffic == "red"){
+    console.log("the light is red log stop");
+ }else if(traffic == "yellow"){
+    console.log("the light is yellow, log Get ready to move.");
+ }else if(traffic == "green"){
+    console.log("the light is green, log Go.");
+ }else {
+    console.log("the input doesn't match any color, log Invalid input.")}
+ 
+
+// 2.Weather Description (Temperature Descriptions)
+// Write a program that describes the temperature:
+
+// If the temperature is exactly 0, log "It's freezing cold."
+// If the temperature is exactly 15, log "It's a cool day."
+// If the temperature is exactly 25, log "It's a pleasant day."
+// If the temperature doesn't match any of these, log "Unknown weather."
+var weather = "temparature";
+ if(weather == "temparature-0"){
+    console.log("the temparature is exactly 0, log It's freezing cold.");
+ }else if(weather == "temparature 15"){
+    console.log(" the temperature is exactly 15, log It's a cool day.")
+ }else if(weather =="temparature -25"){
+    console.log("the temperature is exactly 25, log It's a pleasant day.")
+ }else{
+    console.log("the temperature doesn't match any of these, log Unknown weather.")
+ }
+
+// 3.: Day of the Week
+
+// Create a program that takes input of a number representing the day of the week (1-7):
+
+// If the number is 1, log "Today is Monday."
+// If the number is 2, log "Today is Tuesday."
+// If the number is 3, log "Today is Wednesday."
+// If the number is 4, log "Today is Thursday."
+// If the number is 5, log "Today is Friday."
+// If the number is 6, log "Today is Saturday."
+// If the number is 7, log "Today is Sunday."
+// If the input is not between 1 and 7, log "Invalid day number."
+   var days= "1";
+   if( days == "ddgs"){
+    console.log("Today is Monday.");
+   }else if ( days == "2"){
+    console.log( "Today is Tuesday.")
+   }else if(days == "3"){
+    console.log("Today is Wednesday.")
+   }else if(days == "4"){
+    console.log("Today is Thursday.")
+   }else if(days == "5"){
+    console.log("Today is Friday.")
+   }else if(days == "6"){
+    console.log("Today is Saturday.")
+   }else if(days == "7"){
+    console.log("Today is Sunday.")
+   }else{
+    console.log ("the input is not between 1 and 7, log Invalid day number.")
+   }
